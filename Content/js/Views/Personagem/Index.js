d8c6d875db31d72b35a7e42e4ee40a7d0f0716be
CaioMasterCoin <caio.pereira@mastercoin.com.br>
2023-09-26 16:55:27 -0300

    Adicionar arquivos de projeto.

---
+++ Content/js/Views/Personagem/Index.js
@@ -0,0 +1,29 @@
+﻿var appPersonagem = new Vue({
+    el: "#personagemApp",
+    data: {
+        Personagens: [],
+    },
+    methods: {
+        GetPersonagens: function () {
+            var self = this;
+            $.ajax({
+                type: "POST",
+                url: "/Personagem/GetPersonagens",
+                success: function (data) {
+                    if (data.personagens.length) {
+                        self.Personagens = data.personagens;
+                    }
+                },
+                error: function (error) {
+                    toastr.error('Erro ao executar operação.');
+                }
+            });
+        },
+    },
+    mounted: function () {
+        var self = this;
+
+        self.GetPersonagens();
+    },
+
+});
---
+++ Controllers/PersonagemController.cs
@@ -0,0 +1,32 @@
+﻿using NarutoUnicarioca.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NarutoUnicarioca.Controllers
+{
+    public class PersonagemController : Controller
+    {
+        public ActionResult Index()
+        {
+            var personagens = "";
+            ViewBag.Personagens = JsonConvert.SerializeObject(personagens);
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetPersonagens()
+        {
+            var personagens = new List<Personagem>
+            {
+                new Personagem { Id = 1, Name = "Naruto" }
+            };
+
+
+            return Json(new { personagens }, JsonRequestBehavior.DenyGet);
+        }
+    }
+}
